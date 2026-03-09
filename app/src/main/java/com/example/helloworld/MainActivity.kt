7050f78aa57238399aafa692c813bc242c4214b8
ZeroMess1 <lorenquen11@gmail.com>
2026-03-09 10:35:46 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -7,34 +7,34 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    
+
     private var counter = 0
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         val counterText: TextView = findViewById(R.id.counterText)
         val btnIncrement: Button = findViewById(R.id.btnIncrement)
         val btnDecrement: Button = findViewById(R.id.btnDecrement)
         val fabReset: FloatingActionButton = findViewById(R.id.fabReset)
-        
+
         btnIncrement.setOnClickListener {
             counter++
             updateCounter(counterText)
         }
-        
+
         btnDecrement.setOnClickListener {
             counter--
             updateCounter(counterText)
         }
-        
+
         fabReset.setOnClickListener {
             counter = 0
             updateCounter(counterText)
         }
     }
-    
+
     private fun updateCounter(textView: TextView) {
         textView.text = counter.toString()
         textView.setTextColor(
